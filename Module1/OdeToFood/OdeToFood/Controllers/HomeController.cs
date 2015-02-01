ed4053b94094f9f7bd7597438ede944ce6fbede7
caleb666 <caleb.netpol@gmail.com>
2015-02-01 08:53:34 +0100

    first commit

---
+++ Module1/OdeToFood/OdeToFood/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using OdeToFood.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFood.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+          //  ViewBag.Location = "Gliwice, PL";
+
+
+            var model = new AboutModel();
+            model.Name = "Mike";
+            model.Location = "Gliwice";
+
+
+            return View(model);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
